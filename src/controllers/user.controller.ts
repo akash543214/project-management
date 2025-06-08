6b5e63132477fb2c7ecfdadc6990b9ecd62108e2
Akash Sharma <sharma.54akash@gmail.com>
2025-06-08 23:17:52 +0530

    prisma added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,17 +1,19 @@
-
 import { Request, Response } from 'express';
 import { asyncHandler } from '../utils/asyncHandler';
 import { ApiResponse } from '../utils/apiResponse';
-const registerUser = asyncHandler(async(req: Request, res: Response) => {
+import { PrismaClient } from '../generated/prisma/index';
+const prisma = new PrismaClient();
 
-    
+
+const registerUser = asyncHandler(async(req: Request, res: Response) => {
  const { name, email } = req.body;
 
-  const newUser = {
-    id: Date.now().toString(),
-    name,
-    email,
-  };
+  const newUser = await prisma.user.create({
+    data:{
+      name: name,
+      email: email
+    }
+  })
    res.status(201).json(new ApiResponse(200, newUser, "Resgistration successfully"));
 
 });
