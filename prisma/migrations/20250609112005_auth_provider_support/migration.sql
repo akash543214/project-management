9865091acf49fc835a73e3e0eb63b4cdd93ee5d5
Akash Sharma <sharma.54akash@gmail.com>
2025-06-09 17:47:45 +0530

    register controller added with bcrypt

---
+++ prisma/migrations/20250609112005_auth_provider_support/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[googleId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateEnum
+CREATE TYPE "AuthProvider" AS ENUM ('local', 'google');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "googleId" TEXT,
+ADD COLUMN     "password" TEXT,
+ADD COLUMN     "provider" "AuthProvider" NOT NULL DEFAULT 'local',
+ADD COLUMN     "refreshToken" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,19 +2,41 @@ import { Request, Response } from 'express';
 import { asyncHandler } from '../utils/asyncHandler';
 import { ApiResponse } from '../utils/apiResponse';
 import { PrismaClient } from '../generated/prisma/index';
+import ApiError from '../utils/apiError';
+import bcrypt from "bcrypt";
+
 const prisma = new PrismaClient();
 
 const registerUser = asyncHandler(async(req: Request, res: Response) => {
- const { name, email } = req.body;
+ const { name, email, password,provider } = req.body;
 
+  // Check if user already exists
+  const existingUser = await prisma.user.findUnique({ where: { email } });
+  if (existingUser) {
+      throw new ApiError("user already exist",500);
+  }
+
+   let hashedPassword: string | undefined;
+  if (provider === "local") {
+    if (!password) {
+      throw new ApiError("Password is required",400);  
+      }
+    hashedPassword = await bcrypt.hash(password, 12);
+  }
+
+  // Create user
   const newUser = await prisma.user.create({
-    data:{
-      name: name,
-      email: email
-    }
-  })
-   res.status(201).json(new ApiResponse(200, newUser, "Resgistration successfully"));
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      provider,
+    },
+  });
 
+  res.status(200).json(new ApiResponse(200, newUser, "Resgistration successfully"));
+
+   
 });
 
 export {
